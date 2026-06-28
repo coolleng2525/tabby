c22bc13c3adfd057dedcac3ef960c769b2ab7d01
mac <macmini@qq.com>
2026-06-28 19:21:21 +0800

    Use crypto UUID for HubTerm node IDs

--- tabby-hubterm/src/hubterm.service.ts
+++ tabby-hubterm/src/hubterm.service.ts
@@ -77,6 +77,9 @@ export class HubTermService {
     }
 
     private generateId (): string {
+        if (crypto.randomUUID) {
+            return crypto.randomUUID()
+        }
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
             const r = Math.random() * 16 | 0
             return (c === 'x' ? r : r & 0x3 | 0x8).toString(16)
